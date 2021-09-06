7ec5818a8dad51b2efed8779695f39845d7b4d21
James Alday <james.alday@wheelsup.com>
2021-09-05 20:40:30 -0400

    serverless fullstack initial build

---
+++ site/build/precache-manifest.4412ca5167cc4de9ac081495428f68de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff1248ad3ad1d3c6dbf269d4e6b9c33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "768c2b4720c09a1d1d50",
+    "url": "/static/css/main.9af3ca94.chunk.css"
+  },
+  {
+    "revision": "da2e7756250675285e20",
+    "url": "/static/js/2.d39a0a57.chunk.js"
+  },
+  {
+    "revision": "851820dc40bca3bcaf80cf6dd26069f9",
+    "url": "/static/js/2.d39a0a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "768c2b4720c09a1d1d50",
+    "url": "/static/js/main.e893740a.chunk.js"
+  },
+  {
+    "revision": "92140938f21271b78561",
+    "url": "/static/js/runtime-main.f1889a16.js"
+  }
+]);
